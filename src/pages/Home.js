60ff6a693c32cd1fbd4dbadbfa1541af4a9dfc27
fajaru <fajarudewangga@gmail.com>
2019-11-20 07:08:18 +0800

    remake list item with flatlist

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   TouchableNativeFeedback,
   Text,
   View,
+  FlatList,
+  Alert,
 } from 'react-native';
-import {Card, Icon} from 'react-native-elements';
+import {Button, Card, Icon} from 'react-native-elements';
+import Loader from 'react-native-modal-loader';
+import firebase from 'react-native-firebase';
+import dayjs from 'dayjs';
 
 class Home extends React.Component {
+  state = {
+    activities: [],
+    status: true,
+  };
+
   static navigationOptions = ({navigation}) => {
     return {
       title: 'Fajaru',
@@ -18,6 +27,7 @@ class Home extends React.Component {
         marginRight: -6,
         textAlign: 'right',
       },
+
       headerLeft: null,
       headerRight: () => (
         <Icon
@@ -30,191 +40,87 @@ class Home extends React.Component {
     };
   };
 
+  componentDidMount = async () => {
+    try {
+      await firebase
+        .firestore()
+        .collection('activities')
+        .get()
+        .then(i => {
+          this.setState({activities: i.docs.map(doc => doc.data())});
+        });
+      this.setState({status: false});
+    } catch (error) {
+      this.setState({status: false});
+      Alert.alert('Error Get Data', error.message);
+    }
+  };
+
   render() {
     return (
       <SafeAreaView
         style={{
           flex: 1,
-          alignItems: 'center',
           backgroundColor: '#559fd6',
+          position: 'relative',
         }}>
-        <ScrollView style={{width: '100%'}}>
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback>
-            <Card
-              containerStyle={{
-                borderRadius: 4,
-                backgroundColor: '#fafafa',
-                marginBottom: 16,
-              }}
-              wrapperStyle={{
-                flexDirection: 'row',
-                width: '100%',
-              }}>
-              <View
-                style={{
-                  borderRightWidth: 2,
-                  marginRight: 16,
-                  paddingRight: 16,
-                }}>
-                <Icon name="calendar" type="font-awesome" size={48} />
-                <Text style={{marginTop: 6}}>30-08-19</Text>
-              </View>
-              <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
-                4 Activities
-              </Text>
-            </Card>
-          </TouchableNativeFeedback>
-        </ScrollView>
+        <FlatList
+          style={{width: '100%'}}
+          data={this.state.activities}
+          renderItem={({item}) => (
+            <TouchableNativeFeedback
+              onPress={() =>
+                console.log(new Date(item.date.seconds * 1000).toString())
+              }>
+              <Card
+                containerStyle={{
+                  borderRadius: 4,
+                  backgroundColor: '#fafafa',
+                }}
+                wrapperStyle={{
+                  flexDirection: 'row',
+                  width: '100%',
+                }}
+                key={item.id}>
+                <View
+                  style={{
+                    borderRightWidth: 2,
+                    marginRight: 16,
+                    paddingRight: 16,
+                  }}>
+                  <Icon name="calendar" type="font-awesome" size={48} />
+                  <Text style={{marginTop: 6}}>
+                    {dayjs(
+                      new Date(item.date.toDate()).toLocaleDateString('en', {
+                        weekday: null,
+                      }),
+                    ).format('DD/MM/YY')}
+                  </Text>
+                </View>
+                <Text style={{fontSize: 24, textAlignVertical: 'center'}}>
+                  {item.activity}
+                </Text>
+              </Card>
+            </TouchableNativeFeedback>
+          )}
+        />
+        <Button
+          buttonStyle={{
+            backgroundColor: '#3274a5',
+            bottom: 12,
+            left: 16,
+            position: 'absolute',
+            width: '90%',
+          }}
+          icon={{
+            name: 'plus',
+            type: 'font-awesome',
+            size: 24,
+            color: '#fff',
+          }}
+          onPress={() => this.props.navigation.navigate('Add')}
+        />
+        <Loader loading={this.state.status} color="#559fd6" />
       </SafeAreaView>
     );
   }
